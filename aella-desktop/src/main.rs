dc0893c0f94ea3a233e78f09b366bd6518bfcd7f
ARKye03 <rafael.sanchez@lagarsoft.com>
2026-02-14 19:13:38 -0300

    feat: Add platform-specific application icons and update bundle metadata.

--- aella-desktop/src/main.rs
+++ aella-desktop/src/main.rs
@@ -2,13 +2,34 @@ mod app;
 mod db;
 
 use app::{State, app_title, initialize_database_task, update, view};
+use iced::window;
 
 fn main() -> iced::Result {
+    let window_settings = window::Settings {
+        icon: app_icon(),
+        ..window::Settings::default()
+    };
+
     iced::application(
         || (State::default(), initialize_database_task()),
         update,
         view,
     )
     .title(app_title)
+    .window(window_settings)
     .run()
+}
+
+fn app_icon() -> Option<window::Icon> {
+    #[cfg(target_os = "windows")]
+    {
+        let bytes = include_bytes!("../assets/aellaWindowsIcon.ico");
+        return window::icon::from_file_data(bytes, None).ok();
+    }
+
+    #[cfg(not(target_os = "windows"))]
+    {
+        let bytes = include_bytes!("../assets/aellaIcon.png");
+        return window::icon::from_file_data(bytes, None).ok();
+    }
 }
